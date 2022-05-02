f0e020ec6503c34e864ca492c0c11b6afa3bdf09
diego.data.icloud@gmail.com <diego.data.icloud@gmail.com>
2022-05-02 17:50:53 -0300

    Presentacion opcional clase 2

---
+++ js/main.js
@@ -0,0 +1,7 @@
+let propuesta = prompt("Ingrese un Presupuesto");
+while (propuesta != "DECLINO") {
+  //operacion
+  console.log("El presupuesto ofrecido es $ " + propuesta);
+
+  propuesta = prompt("Ingresa otro Presupuesto, Tu cotizacion es muy alta");
+}
